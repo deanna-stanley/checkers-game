d44a25a81065ce1e4857ad5b85f7d0078ea9dcc2
deanna-stanley <dstanley.git@gmail.com>
2025-07-05 21:22:56 -0700

    Fixed jumping, added multiple jumps and making a checker a king

--- js/checkers.js
+++ js/checkers.js
@@ -2,6 +2,8 @@ const checkerArray = [];
 const moveMarkerArray = [];
 const blackCheckerUrl = new URL("../img/black.svg", import.meta.url);
 const redCheckerUrl = new URL("../img/red.svg", import.meta.url);
+const blackKingUrl = new URL("../img/black_king.svg", import.meta.url);
+const redKingUrl = new URL("../img/red_king.svg", import.meta.url);
 const boardDiv = document.querySelector("#checkerBoard");
 const playerTurnHeader = document.querySelector("#playerTurnHeader");
 let playerTurn = "Black";
@@ -36,6 +38,7 @@ function createBoard() {
                     image.style.setProperty("--x", j);
                     image.style.setProperty("--y", i);
                     image.style.setProperty("--color", "black");
+                    image.style.setProperty("--king", "false");
                     image.addEventListener("click", checkerClick);
                     boardDiv.appendChild(image);
                 } else if (i >= 5) { // red checkers
@@ -46,6 +49,7 @@ function createBoard() {
                     image.style.setProperty("--x", j);
                     image.style.setProperty("--y", i);
                     image.style.setProperty("--color", "red");
+                    image.style.setProperty("--king", "false");
                     image.addEventListener("click", checkerClick);
                     boardDiv.appendChild(image);
                 } else { // no checkers
@@ -78,59 +82,103 @@ function checkerClick(event) {
         clearMoveMarkers();    
         }
         checkerClicked = checker;
-        possibleMoves(checkerColor, checkerX, checkerY);
+        possibleMoves(checkerColor, checkerX, checkerY, false);
     }
 }
 
-function possibleMoves(checkerColor, x, y) {
-    const newX = x + 1;
-    const newX2 = x - 1;
-    let newY;
-    let result;
-    let result2;
+function possibleMoves(checkerColor, x, y, jumpsOnly) {
+    let newXPlus = x + 1;
+    let newXMinus = x - 1;
+    let yDirection;
+    let jumpX;
+    let jumpY;
+    let resultPlus = {
+        Empty: null,
+        Color: null
+    };
+    let resultMinus = {
+        Empty: null,
+        Color: null
+    };
 
     if (checkerColor === "black") {
-        newY = y + 1;
+        yDirection = 1;
     } else {
-        newY = y - 1;
+        yDirection = -1;
     }
+
+    const newY = y + yDirection;
 
     if (x === 0) {
         // can only move to x + 1, y + yDirection
         // console.log("Only one possible move.");
-        result = isSquareEmpty(newX, newY);
-        // console.log(result);
+        resultPlus = isSquareEmpty(newXPlus, newY);
+        // console.log(resultPlus);
     } else if (x === 7) {
         // can only move to x - 1, y + yDirection
         // console.log("Only one possible move.");
-        result2 = isSquareEmpty(newX2, newY);
-        // console.log(newX2, newY)
-        // console.log(result2);
+        resultMinus = isSquareEmpty(newXMinus, newY);
+        // console.log(newXMinus, newY)
+        // console.log(resultMinus);
     } else {
         // can move to x + 1, y + yDirection
         // OR to x - 1, y + yDirection
         // console.log("Two possible moves.");
-        result = isSquareEmpty(newX, newY);
-        // console.log(`x + 1 result: ${result}`);
-        result2 = isSquareEmpty(newX2, newY);
-        // console.log(`x - 1 result: ${result}`);
+        resultPlus = isSquareEmpty(newXPlus, newY);
+        // console.log(`x + 1 resultPlus: ${resultPlus}`);
+        resultMinus = isSquareEmpty(newXMinus, newY);
+        // console.log(`x - 1 resultMinus: ${resultMinus}`);
     }
 
-    if (result) {
-        createMoveMarker(newX, newY);
+    if (resultPlus.Empty && !jumpsOnly) {
+        createMoveMarker(newXPlus, newY);
+    } else if ((resultPlus.Empty === false) && (checkerColor !== resultPlus.Color)) {
+        if (!(newXPlus === 0 || newXPlus === 7)) { //TODO: also check for y of 0 or 7
+            jumpY = newY + yDirection;
+            jumpX = newXPlus + 1;
+            let jumpResult = isSquareEmpty(jumpX, jumpY);
+            if (jumpResult.Empty) {
+                createMoveMarker(jumpX, jumpY);
+            }
+            // console.log("check for jump");
+        }
     }
 
-    if (result2) {
-        createMoveMarker(newX2, newY);
+    if (resultMinus.Empty && !jumpsOnly) {
+        createMoveMarker(newXMinus, newY);
+    } else if ((resultMinus.Empty === false) && (checkerColor !== resultMinus.Color)) {
+        if (!(newXMinus === 0 || newXMinus === 7)) { //TODO: also check for y of 0 or 7
+            jumpY = newY + yDirection;
+            jumpX = newXMinus - 1;
+            let jumpResult = isSquareEmpty(jumpX, jumpY);
+            if (jumpResult.Empty) {
+                createMoveMarker(jumpX, jumpY);
+            }
+        }
+        // console.log("check for jump");
     }
 }
 
 function isSquareEmpty(x, y) {
+    let empty;
+    let checkerColor;
+    
     if (checkerArray[y][x] === null) {
-        return true;
+        empty = true;
+        checkerColor = null;
     } else {
-        return false;
+        empty = false;
+        // console.log(checkerArray[y][x]);
+        const checker = checkerArray[y][x];
+        // console.log(checker.style.getPropertyValue("--color"));
+        checkerColor = checker.style.getPropertyValue("--color");
+        // console.log(checkerColor);
     }
+
+    return {
+        Empty: empty,
+        Color: checkerColor
+    };
 }
 
 function createMoveMarker(x, y) {
@@ -152,30 +200,82 @@ function clearMoveMarkers() {
 }
 
 function moveMarkerClick(event) {
-    // TODO: add code to move checker to new position
+    
+    clearMoveMarkers();
+    
+    const checkerColor = checkerClicked.style.getPropertyValue("--color");
+
+    // Move checker to new position
     const marker = event.target;
     const checkerX = parseInt(checkerClicked.style.getPropertyValue("--x"));
     const checkerY = parseInt(checkerClicked.style.getPropertyValue("--y"));
     const checkerNewX = parseInt(marker.style.getPropertyValue("--x"));
     const checkerNewY = parseInt(marker.style.getPropertyValue("--y"));
-
-    clearMoveMarkers();
-    checkerArray[checkerNewY][checkerNewX] = checkerArray[checkerY][checkerX];
-    checkerArray[checkerY][checkerX] = null
+    
+    checkerArray[checkerNewY][checkerNewX] = checkerArray[checkerY][checkerX]; // TODO: Fix this
+    checkerArray[checkerY][checkerX] = null;
     checkerClicked.style.setProperty("--x", checkerNewX);
     checkerClicked.style.setProperty("--y", checkerNewY);
-    checkerClicked = null;
     
+    // If move was a jump remove the jumped checker
+    const xDiff = checkerNewX - checkerX;
+    const yDiff = checkerNewY - checkerY;
+    let checkerRemoveX;
+    let checkerRemoveY;
+    if (xDiff === 2) {
+        checkerRemoveX = checkerX + 1;        
+    } else if (xDiff === -2) {
+        checkerRemoveX = checkerX - 1;
+    }
+    if (yDiff === 2) {
+        checkerRemoveY = checkerY + 1;        
+    } else if (yDiff === -2) {
+        checkerRemoveY = checkerY - 1;
+    }
+    if (Math.abs(xDiff) === 2) {
+        const checkerToRemove = checkerArray[checkerRemoveY][checkerRemoveX];
+        checkerToRemove.parentNode.removeChild(checkerToRemove);
+        checkerArray[checkerRemoveY][checkerRemoveX] = null;
+
+        possibleMoves(checkerColor, checkerNewX, checkerNewY, true);
+    }
+    
+    if (moveMarkerArray.length === 0) {
+        if ((checkerNewY === 7 && checkerColor === 'black') || (checkerNewY === 0 && checkerColor === 'red')) {
+            makeKing(checkerColor);
+        }
+        
+        checkerClicked = null;
+    
+        changeTurns();
+    }
+    
+}
+
+function changeTurns() {
     if (playerTurn === "Black") {
         playerTurn = "Red";
-        
     } else {
         playerTurn = "Black";
     }
     playerTurnHeaderText = `${playerTurn}'s Turn`;
     playerTurnHeader.textContent = playerTurnHeaderText;
     playerTurnHeader.style.color = playerTurn.toLowerCase();
-} 
+}
+
+function makeKing(checkerColor) {
+    const checkerIsKing = checkerClicked.style.getPropertyValue("--king");
+    if (checkerIsKing === "false") {
+        checkerClicked.style.setProperty("--king", "true");
+        if (checkerColor === "black") {
+            checkerClicked.src = blackKingUrl;
+            checkerClicked.alt = "black king checker";
+        } else if (checkerColor === "red") {
+            checkerClicked.src = redKingUrl;
+            checkerClicked.alt = "red king checker";
+        }
+    }
+}
 
 createBoard();
 playerTurnHeader.textContent = playerTurnHeaderText;
